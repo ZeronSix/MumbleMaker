203e00b4378bf86e93a437a1807584668db3c663
ZeronSix <zeronsix@gmail.com>
2013-12-06 21:56:57 +0600

    First commit.

---
+++ MumbleMaker-windows/AboutBox.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows; 
+using System.Windows.Controls; 
+using System.Windows.Input; 
+
+namespace MumbleMaker {
+    /// <summary>
+    /// Interaction logic for AboutBox.xaml
+    /// </summary>
+    public partial class AboutBox : Window {
+        public AboutBox() {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+    }
+}
